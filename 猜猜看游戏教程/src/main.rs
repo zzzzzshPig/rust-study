5c849d78917e50e313703cbc993bba436597e792
zsh <1012352205@qq.com>
2021-12-21 00:12:48 +0800

    feat: 衍生石头剪刀布小游戏

--- 猜猜看游戏教程/src/main.rs
+++ 猜猜看游戏教程/src/main.rs
@@ -1,34 +1,52 @@
 use std::io;
 use rand::Rng;
-use std::cmp::Ordering;
 
 fn main() {
-    println!("欢迎来到猜猜看小游戏！");
-    println!("请输入您认为正确的数字，如果输入错了，系统会提示您是大了还是小了");
-
-    // 生产随机数
-    let seed = rand::thread_rng().gen_range(1..101);
+    println!("欢迎来到石头剪刀布小游戏！");
 
     loop {
-        // 获取用户输入
+        println!("请输入您的决定");
+
+        // 用户出
         let mut user_input = String::new();
         io::stdin().read_line(&mut user_input).expect("啊哦，出错啦！");
-        let user_input: u32 = match user_input.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("请输入数字");
-                continue
-            }
-        };
+        user_input = user_input.trim().to_string();
+        let input_to_num;
 
-        // 比较数字
-        match user_input.cmp(&seed) {
-            Ordering::Greater => println!("大了"),
-            Ordering::Less => println!("小了"),
-            Ordering::Equal => {
-                println!("正确");
-                break;
-            }
+        if user_input == "石头" {
+            input_to_num = 0
+        } else if user_input == "剪刀" {
+            input_to_num = 1
+        } else if user_input == "布" {
+            input_to_num = 2
+        } else {
+            println!("请输入有效值");
+            continue
+        }
+
+        // 电脑出
+        let seed = rand::thread_rng().gen_range(0..3);
+        let seed_str;
+
+        if seed == 0 {
+            seed_str = "石头"
+        } else if seed == 1 {
+            seed_str = "剪刀"
+        } else {
+            seed_str = "布"
+        }
+
+        println!("电脑出 {}", seed_str);
+
+        let win_or_loss = input_to_num - seed;
+
+        if input_to_num == seed {
+            println!("平局")
+        } else if win_or_loss == -1 || win_or_loss == 2 {
+            println!("您赢了");
+            break
+        } else {
+            println!("您输了")
         }
     }
 }
